207c3017a5bedc0dece8f5019f4c518ca022c11b
sunhomj <55496992+sunhomj@users.noreply.github.com>
2019-12-16 21:56:54 +1030

    Add Navbar, color and title styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
+import Navbar from './Navbar/Navbar'
+
 const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Open Sans', sans-serif;
+    margin:0;
   }
   h1,h2,h3{
     font-family: 'Righteous', cursive;
@@ -12,8 +15,8 @@ const GlobalStyle = createGlobalStyle`
 function App() {
   return (
     <>
-      <h1>Sunny Pizza</h1>
       <GlobalStyle whiteColor />
+      <Navbar/>
       <div>hellow Sunny</div>
     </>
   );
