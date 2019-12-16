4ac9bb65d7d229e755a8e1931afe26bc6b5a4611
sunhomj <55496992+sunhomj@users.noreply.github.com>
2019-12-16 21:58:46 +1030

    change sytle colors and add title sytles

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import { pizzaRed } from '../Styles/colors';
+import { Title } from '../Styles/title';
+const NavbarStyled = styled.div`
+	background: ${pizzaRed};
+	padding: 10px;
+`;
+
+const Logo = styled(Title)`
+text-shadow: 1px 3px 1px orange;
+font-size: 20px;
+color:white;
+`;
+const Navbar = () => {
+	return (
+		<NavbarStyled>
+			<Logo>Sunny pizza 🍕</Logo>
+		</NavbarStyled>
+	);
+};
+
+export default Navbar;
---
+++ src/Styles/title.js
@@ -0,0 +1,5 @@
+import sytled from 'styled-components';
+
+export const Title = sytled.div`
+    font-family: 'Righteous', cursive;
+`;
