53809e0b72f6409e50d1a5f551e9b175cb8605a0
sunhomj <55496992+sunhomj@users.noreply.github.com>
2019-12-17 21:33:46 +1030

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
+import { GlobalStyle } from './Styles/GlobalStyle'
 import { Navbar } from './Navbar/Navbar'
 import { Banner } from './Banner/Banner'
 import { Menu } from './Menu/Menu'
 import ScrollToTop from './ScrollToTop/ScrollToTop'
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Open Sans', sans-serif;
-    margin:0;
-  }
-  h1,h2,h3{
-    font-family: 'Righteous', cursive;
-  }
-`;
+
 
 function App() {
   return (
